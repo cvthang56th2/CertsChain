90f48948f23fe08c181e24f8becb62eee378e7b1
thang.cao <cvthang56th2@gmail.com>
2022-08-13 19:06:56 +0700

    up

--- web/src/pages/Home.vue
+++ web/src/pages/Home.vue
@@ -5,19 +5,19 @@
       <div class="w-full xl:w-1/3 px-10">
         <div class="text-center cursor-pointer p-4 border-2 hover:shadow-md" @click="goTo('/issuer')">
           <div class="w-[200px] h-[200px] rounded-full border-4 mx-auto bg bg-center bg-cover bg-no-repeat" style="background-image: url(/issuer-icon.png)"></div>
-          <h5 class="mt-5 text-xl font-semibold uppercase">Issuer</h5>
+          <h5 class="mt-5 text-2xl font-bold uppercase">Issuer</h5>
         </div>
       </div>
       <div class="w-full xl:w-1/3 px-10">
         <div class="text-center cursor-pointer p-4 border-2 hover:shadow-md" @click="goTo('/holder')">
           <div class="w-[200px] h-[200px] rounded-full border-4 mx-auto bg bg-center bg-cover bg-no-repeat" style="background-image: url(/holder-icon.png)"></div>
-          <h5 class="mt-5 text-xl font-semibold uppercase">Holder</h5>
+          <h5 class="mt-5 text-2xl font-bold uppercase">Holder</h5>
         </div>
       </div>
       <div class="w-full xl:w-1/3 px-10">
         <div class="text-center cursor-pointer p-4 border-2 hover:shadow-md" @click="goTo('/verifier')">
           <div class="w-[200px] h-[200px] rounded-full border-4 mx-auto bg bg-center bg-cover bg-no-repeat" style="background-image: url(/verifier-icon.png)"></div>
-          <h5 class="mt-5 text-xl font-semibold uppercase">Verifier</h5>
+          <h5 class="mt-5 text-2xl font-bold uppercase">Verifier</h5>
         </div>
       </div>
     </div>
