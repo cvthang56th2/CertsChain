46acf36c56a4ac627a6dc5a547f3603e9342f7b1
thang.cao <cvthang56th2@gmail.com>
2022-10-30 17:54:19 +0700

    update UI

--- web/src/pages/Instruction.vue
+++ web/src/pages/Instruction.vue
@@ -4,19 +4,21 @@
       <button @click="goTo('/')" class="xl:absolute top-2 underline text-blue-700">&#8592; Back to Home Page</button>
     </div>
     <h2 class="text-center text-2xl font-extrabold mb-4">Instruction</h2>
-    <div class="mt-2">
-      Xem hướng dẫn của người dùng loại:
-      <button class="text-blue-500 ml-5 underline" @click="goTo('/instruction/issuer')">Issuer</button>
-      <button class="text-blue-500 ml-5 underline" @click="goTo('/instruction/holder')">Holder</button>
-      <button class="text-blue-500 ml-5 underline" @click="goTo('/instruction/verifier')">Verifier</button>
-    </div>
-    <div class="mt-2">
-      Hệ thống được thiết kế và phân ra 3 loại người dùng: Issuer, Holder và Verifier. Khi mới vào trang chủ, User cần chọn chức năng của mình đối với hệ thống để tiếp tục sử dụng.
-      <img src="../assets/image/home.png" alt="home" class="mx-auto">
-    </div>
-    <div class="mt-2">
-      Nếu User chọn là Issuer hay Holder, người dùng cần phải đăng nhập vào tài khoản của mình để sử dụng các chức năng của Issuer, Holder.
-      <img src="../assets/image/login.png" alt="login" class="mx-auto">
+    <div class="px-5 pt-5 pb-20 shadow-lg rounded-lg">
+      <div class="mt-3">
+        Xem hướng dẫn của người dùng loại:
+        <button class="text-blue-500 ml-5 underline" @click="goTo('/instruction/issuer')">Issuer</button>
+        <button class="text-blue-500 ml-5 underline" @click="goTo('/instruction/holder')">Holder</button>
+        <button class="text-blue-500 ml-5 underline" @click="goTo('/instruction/verifier')">Verifier</button>
+      </div>
+      <div class="mt-3">
+        Hệ thống được thiết kế và phân ra 3 loại người dùng: Issuer, Holder và Verifier. Khi mới vào trang chủ, User cần chọn chức năng của mình đối với hệ thống để tiếp tục sử dụng.
+        <img src="../assets/image/home.png" alt="home" class="mx-auto">
+      </div>
+      <div class="mt-3">
+        Nếu User chọn là Issuer hay Holder, người dùng cần phải đăng nhập vào tài khoản của mình để sử dụng các chức năng của Issuer, Holder.
+        <img src="../assets/image/login.png" alt="login" class="mx-auto">
+      </div>
     </div>
   </div>
 </template>
--- web/src/pages/InstructionIssuer.vue
+++ web/src/pages/InstructionIssuer.vue
@@ -4,130 +4,130 @@
       <button @click="goTo('/instruction')" class="xl:absolute top-2 underline text-blue-700">&#8592; Back to Instruction</button>
     </div>
     <h2 class="text-center text-2xl font-extrabold mb-4">Issuer's Instruction</h2>
-    
-    <div class="mt-2">
-      Page:
-      <button v-for="page of pages" class="ml-5 cursor-pointer" :class="currentPage === page ? 'bg-blue-800 text-white px-2 py-1' : 'text-blue-500'" @click="currentPage = page">{{page}}</button>
+    <div class="px-5 pt-5 pb-20 shadow-lg rounded-lg">
+      <div class="mt-3">
+        Page:
+        <button v-for="page of pages" class="ml-5 cursor-pointer" :class="currentPage === page ? 'bg-blue-800 text-white px-2 py-1' : 'text-blue-500'" @click="currentPage = page">{{page}}</button>
+      </div>
+      <template v-if="currentPage === 'Profile'">
+        <div class="mt-3">
+          Đây là trang thông tin cá nhân của Issuer, có thể thực hiện chỉnh sửa các thông tin cá nhân của mình tại trang này bằng cách click vào button Edit. 
+          <img src="../assets/image/issuer/profile.png" class="mx-auto">
+          <div class="text-center text-sm italic">Trang Profile của Issuer</div>
+        </div>
+      </template>
+      <template v-if="currentPage === 'Users'">
+        <div class="mt-3">
+          Giúp Issuer có thể quản lý User trong hệ thống:
+          <img src="../assets/image/issuer/user-list.png" class="mx-auto">
+          <div class="text-center text-sm italic">Trang Users của Issuer</div>
+        </div>
+        <div class="mt-3">
+          Issuer có thể thêm user vào bằng cách click vào button Add user, nhập đầy đủ thông tin của user và click vào button Save Changes, nếu thành công sẽ tạo ra một user mới và hiển thị ngay lập tức trên trang danh sách User
+          <img src="../assets/image/issuer/user-add.png" class="mx-auto">
+          <div class="text-center text-sm italic">Popup tạo mới User</div>
+        </div>
+        <div class="mt-3">
+          Issuer có thể cập nhật thông tin User bằng cách click vào button Edit:
+          <img src="../assets/image/issuer/user-edit.png" class="mx-auto">
+          <div class="text-center text-sm italic">Popup cập nhật User</div>
+        </div>
+        
+        <div class="mt-3">
+          Issuer có thể cập nhật danh sách Cources của User bằng cách click vào button Update Cources:
+          <img src="../assets/image/issuer/user-update-cources.png" class="mx-auto">
+          <div class="text-center text-sm italic">Popup Cập nhật danh sách danh sách Cources của User tham gia</div>
+        </div>
+        
+        <div class="mt-3">
+          Issuer có thể cập nhật trạng thái hoạt động của User bằng cách click vào switch button Active/Archived trên trang danh sách Users
+        </div>
+  
+      </template>
+      <template v-if="currentPage === 'Schools and Cources'">
+        <div class="mt-3">
+          Giúp Issuer có thể quản lý trường học và khóa học trong hệ thống:
+          <img src="../assets/image/issuer/school-cource-list.png" class="mx-auto">
+          <div class="text-center text-sm italic">
+            Trang Schools and Cources của Issuer
+          </div>
+        </div>
+        <div class="mt-3">
+          Issuer có thể tạo mới thông tin trường học, khóa học  bằng cách click vào button Add School 
+          <img src="../assets/image/issuer/school-cource-add.png" class="mx-auto">
+          <div class="text-center text-sm italic">
+            Popup tạo mới School and Cource
+          </div>
+        </div>
+        <div class="mt-3">
+          Issuer có thể cập nhật thông tin trường học, khóa học  bằng cách click vào button Edit
+          <img src="../assets/image/issuer/school-cource-edit.png" class="mx-auto">
+          <div class="text-center text-sm italic">
+            Popup cập nhật School and Cource
+          </div>
+        </div>
+        <div class="mt-3">
+          Issuer có thể cập nhật trạng thái hoạt động của trường học, khóa học bằng cách click vào switch button Active/Archived trên trang danh sách Schools and Cources
+        </div>
+        
+      </template>
+      <template v-if="currentPage === 'Certificates'">
+        <div class="mt-3">
+          Giúp Issuer có thể quản lý các VBCC trong hệ thống:
+          Issuer có thể tạo mới thông tin trường học, khóa học  bằng cách click vào button Add School 
+          <img src="../assets/image/issuer/certificate-list.png" class="mx-auto">
+          <div class="text-center text-sm italic">
+            Trang Certificates của Issuer
+          </div>
+        </div>
+        <div class="mt-3">
+          Issuer có thể tạo mới VBCC bằng cách click vào button Add Certificate
+          <img src="../assets/image/issuer/certificate-add.png" class="mx-auto">
+          <div class="text-center text-sm italic">
+            Popup tạo mới Certificate
+          </div>
+        </div>
+        
+        <div class="mt-3">
+          Tại Popup tạo mới Certificate có thể tạo mới một chứng chỉ cho 1 User duy nhất bằng cách chọn User, trường và khóa học của User có tham gia và nhấn Save Changes. Ngoài ra, Issuer có thể cấp phát cho tất cả học viên trong khóa học bằng cách click vào switch button Is Generate for Cource?, chọn khóa học muốn cấp phát và click Save Changes.
+          <img src="../assets/image/issuer/certificate-add-for-cource.png" class="mx-auto">
+          <div class="text-center text-sm italic">
+            Cấp nhát VBCC cho học viên trong khóa học
+          </div>
+        </div>
+        
+        <div class="mt-3">
+          Sau khi nhập đầy đủ thông tin và click Save Changes, hệ thống sẽ tiến hành tạo mới VBCC, sinh ra một file PDF có định dạng như sau:
+          <img src="../assets/image/issuer/certificate-sample.png" class="mx-auto">
+          <div class="text-center text-sm italic">
+            Mẫu văn bằng chứng chỉ của CertsChain
+          </div>
+        </div>
+        
+        <div class="mt-3">
+          Khi VBCC cần được cập nhật trạng thái, Issuer sẽ click vào switch button Active/Archived trên trang danh sách Certificates.
+        </div>
+      </template>
+      <template v-if="currentPage === 'Certificate Requests'">
+        
+        <div class="mt-3">
+          Giúp Issuer có thể quản lý các yêu cầu cấp phát VBCC trong hệ thống:
+          <img src="../assets/image/issuer/cert-request-list.png" class="mx-auto">
+          <div class="text-center text-sm italic">
+            Trang Certificate Requests của Issuer
+          </div>
+        </div>
+        
+        <div class="mt-3">
+          Sau khi xét duyệt yêu cầu cấp phát VBCC, Issuer sẽ click vào button Approve nếu chấp thuận, Deny nếu từ chối yêu cầu cấp phát.
+        </div>
+        
+        <div class="mt-3">
+          Sau khi click vào button Approve, một VBCC mới sẽ được tạo ra và lưu vào hệ thống.
+        </div>
+      </template>
     </div>
-    <template v-if="currentPage === 'Profile'">
-      <div class="mt-2">
-        Đây là trang thông tin cá nhân của Issuer, có thể thực hiện chỉnh sửa các thông tin cá nhân của mình tại trang này bằng cách click vào button Edit. 
-        <img src="../assets/image/issuer/profile.png" class="mx-auto">
-        <div class="text-center text-sm italic">Trang Profile của Issuer</div>
-      </div>
-    </template>
-    <template v-if="currentPage === 'Users'">
-      <div class="mt-2">
-        Giúp Issuer có thể quản lý User trong hệ thống:
-        <img src="../assets/image/issuer/user-list.png" class="mx-auto">
-        <div class="text-center text-sm italic">Trang Users của Issuer</div>
-      </div>
-      <div class="mt-2">
-        Issuer có thể thêm user vào bằng cách click vào button Add user, nhập đầy đủ thông tin của user và click vào button Save Changes, nếu thành công sẽ tạo ra một user mới và hiển thị ngay lập tức trên trang danh sách User
-        <img src="../assets/image/issuer/user-add.png" class="mx-auto">
-        <div class="text-center text-sm italic">Popup tạo mới User</div>
-      </div>
-      <div class="mt-2">
-        Issuer có thể cập nhật thông tin User bằng cách click vào button Edit:
-        <img src="../assets/image/issuer/user-edit.png" class="mx-auto">
-        <div class="text-center text-sm italic">Popup cập nhật User</div>
-      </div>
-      
-      <div class="mt-2">
-        Issuer có thể cập nhật danh sách Cources của User bằng cách click vào button Update Cources:
-        <img src="../assets/image/issuer/user-update-cources.png" class="mx-auto">
-        <div class="text-center text-sm italic">Popup Cập nhật danh sách danh sách Cources của User tham gia</div>
-      </div>
-      
-      <div class="mt-2">
-        Issuer có thể cập nhật trạng thái hoạt động của User bằng cách click vào switch button Active/Archived trên trang danh sách Users
-      </div>
-
-    </template>
-    <template v-if="currentPage === 'Schools and Cources'">
-      <div class="mt-2">
-        Giúp Issuer có thể quản lý trường học và khóa học trong hệ thống:
-        <img src="../assets/image/issuer/school-cource-list.png" class="mx-auto">
-        <div class="text-center text-sm italic">
-          Trang Schools and Cources của Issuer
-        </div>
-      </div>
-      <div class="mt-2">
-        Issuer có thể tạo mới thông tin trường học, khóa học  bằng cách click vào button Add School 
-        <img src="../assets/image/issuer/school-cource-add.png" class="mx-auto">
-        <div class="text-center text-sm italic">
-          Popup tạo mới School and Cource
-        </div>
-      </div>
-      <div class="mt-2">
-        Issuer có thể cập nhật thông tin trường học, khóa học  bằng cách click vào button Edit
-        <img src="../assets/image/issuer/school-cource-edit.png" class="mx-auto">
-        <div class="text-center text-sm italic">
-          Popup cập nhật School and Cource
-        </div>
-      </div>
-      <div class="mt-2">
-        Issuer có thể cập nhật trạng thái hoạt động của trường học, khóa học bằng cách click vào switch button Active/Archived trên trang danh sách Schools and Cources
-      </div>
-      
-    </template>
     
-    <template v-if="currentPage === 'Certificates'">
-      <div class="mt-2">
-        Giúp Issuer có thể quản lý các VBCC trong hệ thống:
-        Issuer có thể tạo mới thông tin trường học, khóa học  bằng cách click vào button Add School 
-        <img src="../assets/image/issuer/certificate-list.png" class="mx-auto">
-        <div class="text-center text-sm italic">
-          Trang Certificates của Issuer
-        </div>
-      </div>
-      <div class="mt-2">
-        Issuer có thể tạo mới VBCC bằng cách click vào button Add Certificate
-        <img src="../assets/image/issuer/certificate-add.png" class="mx-auto">
-        <div class="text-center text-sm italic">
-          Popup tạo mới Certificate
-        </div>
-      </div>
-      
-      <div class="mt-2">
-        Tại Popup tạo mới Certificate có thể tạo mới một chứng chỉ cho 1 User duy nhất bằng cách chọn User, trường và khóa học của User có tham gia và nhấn Save Changes. Ngoài ra, Issuer có thể cấp phát cho tất cả học viên trong khóa học bằng cách click vào switch button Is Generate for Cource?, chọn khóa học muốn cấp phát và click Save Changes.
-        <img src="../assets/image/issuer/certificate-add-for-cource.png" class="mx-auto">
-        <div class="text-center text-sm italic">
-          Cấp nhát VBCC cho học viên trong khóa học
-        </div>
-      </div>
-      
-      <div class="mt-2">
-        Sau khi nhập đầy đủ thông tin và click Save Changes, hệ thống sẽ tiến hành tạo mới VBCC, sinh ra một file PDF có định dạng như sau:
-        <img src="../assets/image/issuer/certificate-sample.png" class="mx-auto">
-        <div class="text-center text-sm italic">
-          Mẫu văn bằng chứng chỉ của CertsChain
-        </div>
-      </div>
-      
-      <div class="mt-2">
-        Khi VBCC cần được cập nhật trạng thái, Issuer sẽ click vào switch button Active/Archived trên trang danh sách Certificates.
-      </div>
-    </template>
-    
-    <template v-if="currentPage === 'Certificate Requests'">
-      
-      <div class="mt-2">
-        Giúp Issuer có thể quản lý các yêu cầu cấp phát VBCC trong hệ thống:
-        <img src="../assets/image/issuer/cert-request-list.png" class="mx-auto">
-        <div class="text-center text-sm italic">
-          Trang Certificate Requests của Issuer
-        </div>
-      </div>
-      
-      <div class="mt-2">
-        Sau khi xét duyệt yêu cầu cấp phát VBCC, Issuer sẽ click vào button Approve nếu chấp thuận, Deny nếu từ chối yêu cầu cấp phát.
-      </div>
-      
-      <div class="mt-2">
-        Sau khi click vào button Approve, một VBCC mới sẽ được tạo ra và lưu vào hệ thống.
-      </div>
-    </template>
   </div>
 </template>
 
